638ee6e5a479322b8ce62881cc60c369d872ad13
msgloriayang <yanggloria91@gmail.com>
2023-03-26 11:47:51 -0500

    added const inquirer and question prompt in index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,34 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      message: "What's the project title?",
+      name: 'title',
+      validate: (value) => {
+        if (value) {
+          return true;
+        } else {
+          return 'Please enter a project title.';
+        }
+      },
+    },
+  ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    // TODO: implement function
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+        // TODO: call writeToFile function and pass in answers
+}
 
 // Function call to initialize app
 init();
