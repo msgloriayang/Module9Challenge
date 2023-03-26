459170511dedb63034c71e246be04996c82203bb
msgloriayang <yanggloria91@gmail.com>
2023-03-26 12:11:03 -0500

    made some changes to index.js. added questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,34 +1,80 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      message: "What's the project title?",
-      name: 'title',
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return 'Please enter a project title.';
-        }
+        type: "input",
+        name: "title",
+        message: "Please name your Project.",
       },
-    },
-  ];
+      {
+        type: "input",
+        name: "description",
+        message: "Please describe the purpose and functionality of this project.",
+      },
+      {
+        type: "input",
+        name: "screenshot",
+        message: "Please provide the relative path to the image you want to use as the screenshot."
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Please provide a URL where a user can access your deployed application."
+      },
+      {
+        type: "input",
+        name: "require",
+        message: "List any project dependencies here.",
+      },
+      {
+        type: "input",
+        name: "features",
+        message: "List some cool features about this project here.",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message:
+          "State the languages or technologies associated with this project.",
+      },
+      {
+        type: "input",
+        name: "creator",
+        message: "Write your GitHub username.",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Provide a valid email address.",
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: "Please list any contributors. (Use GitHub usernames)",
+        default: "",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "Provide walkthrough of required tests if applicable.",
+      },
+    ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // TODO: implement function
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        console.log(answers);
+    inquirer.prompt(questions).then((responses) => {
+      console.log("Creating Professional README.md File...");
+      writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
     });
-}
-
-// Function call to initialize app
-init();
+  }
+  init();
